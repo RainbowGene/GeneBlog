579b1de2b07298941973f41fcc5d88068f69fc5c
Gene <1767386755@qq.com>
2020-07-28 16:37:25 +0800

    admin 初始化

---
+++ blog-admin/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <div>
+      <div class="float-left">Float left on all viewport sizes</div>
+      <br />
+      <div class="float-right">Float right on all viewport sizes</div>
+      <br />
+      <div class="float-none">Don't float on all viewport sizes</div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
